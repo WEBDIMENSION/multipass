71ee657b9e28d678577971f4883b3c5cd1e486f8
webdimension <office@webdimension.jp>
2022-07-13 20:08:29 +0900

    fix

--- create_vm.sh
+++ create_vm.sh
@@ -1,24 +1,35 @@
 #!/usr/bin/env bash
 
+set -eu
 echo -e -n "\n"
 echo "###################################"
-echo "####   create multipass VM    #####"
+echo "####        shellcheck        #####"
 echo "###################################"
+shellcheck create_vm.sh
 
+echo -e -n "\n"
+echo "###################################"
+echo "####       Read .env          #####"
+echo "###################################"
 source ./.env
 
-pv=$(python --version | awk  -F " "  '{ print $2 }')
+pv=$(python --version | awk -F " " '{ print $2 }')
 echo "####   Check python version    #####"
-echo "python version: "$pv
+echo "python version: $pv"
 echo -e -n "\n"
 
-if [ $pv != "$PYTHON_VERSION" ]; then
-	echo -e -n "\n"
-	echo "###################################"
-	echo "pyenv install $PYTHON_VERSION"
-	echo "###################################"
-	pyenv install "$PYTHON_VERSION"
-	pyenv local "$PYTHON_VERSION"
+if [ "$pv" != "$PYTHON_VERSION" ]; then
+  if [ ! -e ~/.pyenv/versions/"$PYTHON_VERSION" ]; then
+    echo -e -n "\n"
+    echo "###################################"
+    echo "pyenv install $PYTHON_VERSION"
+    echo "###################################"
+    pyenv install "$PYTHON_VERSION"
+  fi
+  echo "###################################"
+  echo "pyenv install local $PYTHON_VERSION"
+  echo "###################################"
+  pyenv local "$PYTHON_VERSION"
 fi
 
 echo -e -n "\n"
@@ -44,7 +55,7 @@ echo -e -n "\n"
 echo "###################################"
 echo "###       ansible-lint          ###"
 echo "###################################"
-ansible-lint  site.yml
+ansible-lint
 
 echo -e -n "\n"
 echo "###################################"
@@ -52,7 +63,7 @@ echo "###       cloud-config          ###"
 echo "###################################"
 AUTHORIZED_KEYS_ID_RSA=$(cat "$RSA_PUB_FILE_NAME")
 AUTHORIZED_KEYS_ANSIBLE_RSA=$(cat "$ANSIBLE_RSA_PUB_FILE_NAME")
-cat > ./cloud-config.yaml << _EOF_
+cat >./cloud-config.yaml <<_EOF_
 ---
 locale: en_US.UTF8
 timezone: Asia/Tokyo
@@ -67,32 +78,35 @@ echo -e -n "\n"
 echo "###################################"
 echo "###         Create VM           ###"
 echo "###################################"
-multipass launch -c "${CPU}" -m "${MEMORY}" -d "${DISK_SIZE}"  -n "${VM_NAME}" "${VM_VERSION}" --cloud-init "${CLOUD_INIT}"
+multipass launch -c "${CPU}" -m "${MEMORY}" -d "${DISK_SIZE}" -n "${VM_NAME}" "${VM_VERSION}" --cloud-init "${CLOUD_INIT}"
 
 echo -e -n "\n"
 echo "###################################"
 echo "###        Mount Dir            ###"
 echo "###################################"
-multipass mount "${HOME}/${MOUNT_WORKSPACE_HOST}" "${VM_NAME}":"${MOUNT_WORKSPACE_VM}"
+multipass mount "${MOUNT_WORKSPACE_HOST}" "${VM_NAME}":"${MOUNT_WORKSPACE_VM}"
+echo "mount ${MOUNT_WORKSPACE_HOST} ${VM_NAME}:${MOUNT_WORKSPACE_VM}"
 
 echo -e -n "\n"
 echo "###################################"
 echo "###      VM IP address          ###"
 echo "###################################"
-vm_ip=$(multipass info develop | grep IPv4 | awk  '{ print $2 }')
-echo "${vm_ip}"
+vm_ip=$(multipass info develop | grep IPv4 | awk '{ print $2 }')
+echo "ipv4 : ${vm_ip}"
 
 echo -e -n "\n"
 echo "###################################"
 echo "###  Create ansible inventory   ###"
 echo "###################################"
-cat > ./hosts/multipass << _EOF_
+cat >./hosts/multipass <<_EOF_
 [multipass]
 multipass ansible_host=$vm_ip ansible_user=ubuntu ansible_ssh_private_key_file=$RSA_FILE_NAME ansible_ssh_common_args='-o StrictHostKeyChecking=no'
 
 [ubuntu20]
 multipass
 _EOF_
+
+cat ./hosts/multipass
 
 echo -e -n "\n"
 echo "###################################"
@@ -101,8 +115,8 @@ echo "###################################"
 
 echo "$DEVELOP_INIT"
 if [ "$DEVELOP_INIT" == 'true' ]; then
-#	ansible-playbook -i hosts/multipass site.yml -l multipass
-	ansible-playbook -i hosts/multipass site.yml -l multipass -t tests
+	ansible-playbook -i hosts/multipass site.yml -l multipass
+#	ansible-playbook -i hosts/multipass site.yml -l multipass -t tests
 else
 	ansible-playbook -i hosts/multipass site.yml -l multipass --skip-tags develop_init
 fi
