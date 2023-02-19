c075b5a339591ce71775d8f1c08a07b5a7a44eff
webdimension <office@webdimension.jp>
2023-02-19 20:07:50 +0900

    Fix

--- create_vm.sh
+++ create_vm.sh
@@ -68,7 +68,7 @@ cat >./cloud-config.yaml <<_EOF_
 locale: en_US.UTF8
 timezone: Asia/Tokyo
 packages:
-  - nfs-common
+#  - nfs-common
 users:
   - name: ubuntu
     ssh-authorized-keys:
@@ -84,12 +84,12 @@ echo "###################################"
 multipass launch -c "${CPU}" -m "${MEMORY}" -d "${DISK_SIZE}" -n "${VM_NAME}" "${VM_VERSION}" --cloud-init "${CLOUD_INIT}"
 sleep 30
 
-#echo -e -n "\n"
-#echo "###################################"
-#echo "###        Mount Dir            ###"
-#echo "###################################"
-#multipass mount "${MOUNT_WORKSPACE_HOST}" "${VM_NAME}":"${MOUNT_WORKSPACE_VM}"
-#echo "mount ${MOUNT_WORKSPACE_HOST} ${VM_NAME}:${MOUNT_WORKSPACE_VM}"
+echo -e -n "\n"
+echo "###################################"
+echo "###        Mount Dir            ###"
+echo "###################################"
+multipass mount "${MOUNT_WORKSPACE_HOST}" "${VM_NAME}":"${MOUNT_WORKSPACE_VM}"
+echo "mount ${MOUNT_WORKSPACE_HOST} ${VM_NAME}:${MOUNT_WORKSPACE_VM}"
 
 echo -e -n "\n"
 echo "###################################"
@@ -124,15 +124,15 @@ cat ./hosts/multipass
 # echo "###################################"
 # multipass exec ${vm_name} -- sudo sh -c  "apt install -y nfs-common"
 
-echo -e -n "\n"
-echo "###################################"
-echo "###           NFS               ###"
-echo "###################################"
-sudo sed -i "" "/${vm_ip}/d" /etc/exports
-sudo sh -c "echo '${MOUNT_WORKSPACE_HOST} alldirs -mapall=${USER_ID}:${GROUP_ID}  ${vm_ip}' >> /etc/exports"
-multipass exec ${VM_NAME} -- mkdir ${MOUNT_WORKSPACE_VM}
-multipass exec ${VM_NAME} -- sudo sh -c "echo '${NFS_HOST_IP}:${MOUNT_WORKSPACE_HOST} ${MOUNT_WORKSPACE_VM} nfs defaults,nofail,noatime,nolock,intr,tcp,actimeo=1800 0 0' >> /etc/fstab"
-multipass exec ${VM_NAME} -- sudo sh -c "mount -t nfs ${NFS_HOST_IP}:${MOUNT_WORKSPACE_HOST} ${MOUNT_WORKSPACE_VM} "
+# echo -e -n "\n"
+# echo "###################################"
+# echo "###           NFS               ###"
+# echo "###################################"
+# sudo sed -i "" "/${vm_ip}/d" /etc/exports
+# sudo sh -c "echo '${MOUNT_WORKSPACE_HOST} alldirs -mapall=${USER_ID}:${GROUP_ID}  ${vm_ip}' >> /etc/exports"
+# multipass exec ${VM_NAME} -- mkdir ${MOUNT_WORKSPACE_VM}
+# multipass exec ${VM_NAME} -- sudo sh -c "echo '${NFS_HOST_IP}:${MOUNT_WORKSPACE_HOST} ${MOUNT_WORKSPACE_VM} nfs defaults,nofail,noatime,nolock,intr,tcp,actimeo=1800 0 0' >> /etc/fstab"
+# multipass exec ${VM_NAME} -- sudo sh -c "mount -t nfs ${NFS_HOST_IP}:${MOUNT_WORKSPACE_HOST} ${MOUNT_WORKSPACE_VM} "
 
 echo "###################################"
 echo "###   Exec ansible playbook     ###"
@@ -141,7 +141,7 @@ echo "###################################"
 echo "$DEVELOP_INIT"
 if [ "$DEVELOP_INIT" == 'true' ]; then
   ansible-playbook -i hosts/multipass site.yml -l multipass
-#	ansible-playbook -i hosts/multipass site.yml -l multipass -t tests
+	ansible-playbook -i hosts/multipass site.yml -l multipass -t tests
 else
   ansible-playbook -i hosts/multipass site.yml -l multipass --skip-tags develop_init
 fi
